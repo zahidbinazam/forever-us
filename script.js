15d6ddc8cc8ff3025d934bd8de2a122d0130bd18
zahidbinazam <zahidbinazam10@gmail.com>
2026-08-02 06:58:42 +0600

    Set default entered code to match secret code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
    1. PASSWORD & LOCK SCREEN SYSTEM
    ========================================================================== */
 const SECRET_CODE = "06"; // Default 2-digit secret password
-let enteredCode = "";
+let enteredCode = "06";
 
 const dot1 = document.getElementById('dot1');
 const dot2 = document.getElementById('dot2');
